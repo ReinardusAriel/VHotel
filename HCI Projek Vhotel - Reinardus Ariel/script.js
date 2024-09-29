be8b6f03c5d26577874c1e7a2a932f5220a34849
ReinardusAriel <reinardusariel31@gmail.com>
2024-09-30 00:04:59 +0700

    Add files via upload

---
+++ HCI Projek Vhotel - Reinardus Ariel/script.js
@@ -0,0 +1,24 @@
+const forms= document.querySelector(".forms"),
+      links= document.querySelector(".signup-link");
+
+console.log(forms, links)
+
+function signinbutton() {
+    window.location.href = "signin.html";
+}
+function roomsbutton() {
+    window.location.href = "rooms.html";
+}
+
+function reservationbutton(){
+    window.location.href = "reservation.html";
+}
+function aboutusbutton(){
+    window.location.href = "aboutus.html";
+}
+function facillitesbutton(){
+    window.location.href = "facilities.html";
+}
+function logobutton(){
+    window.location.href = "vhotel.html";
+}
